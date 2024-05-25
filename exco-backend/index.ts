5090a4ea0cbcd75925f490a4a05b9c8188c5d6d6
Sastik kumar <sastikkumardas@gmail.com>
2024-05-25 18:19:17 +0530

    feat(server): FYEAR-1 add server health chek api

--- exco-backend/index.ts
+++ exco-backend/index.ts
@@ -47,6 +47,10 @@ DBCon.initDatabse(
 DBCon
     .connect()
     .then(() => {
+        app.get('/', (req, res) => {
+            res.status(200).json({ message: 'Server is running properly' });
+        });
+        
         const listener = app.listen(
             process.env.PORT || 3000,
             () => {
